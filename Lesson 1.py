693393e68039ec09e284b5a3a7586dff5381dc11
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2025-09-17 14:01:35 +0300

    Lesson 1

---
+++ Lesson 1.py
@@ -0,0 +1,51 @@
+# Data Types:
+
+# 1. Strings
+part1 = "Hello "
+part2 = 'World'
+
+user_name_of_application = "Shams"
+print(user_name_of_application)
+
+statement = part1 + part2
+
+print(statement)
+
+# 2. Integers
+
+number = 100
+print(number)
+
+# 3. Floats
+pi = 3.14
+print(pi)
+
+# 4. Booleans
+
+answer1 = True # صح
+answer2 = False # خطأ
+
+print(answer1)
+
+# printing several types:
+
+age_of_subhi = 25
+print ("Age of Subhi: " + str(age_of_subhi))
+
+# To know the data type use type() function
+
+# Converting Data Types:
+
+# 1. str() 
+a = str("Subhi")
+print (type(a))
+print(a)
+
+# 2. int()
+print (int("123"))
+
+# 3. float()
+print(type(float("1.618")))
+
+# 4. bool()
+print (bool(0))
