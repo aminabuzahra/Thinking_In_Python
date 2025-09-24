dba825fa70d27af6526a164a62f0e6d771829007
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2025-09-24 12:39:04 +0300

    Lesson 2

---
+++ Lesson_2/Lesson 2.py
@@ -0,0 +1,142 @@
+# name = input("Enter you name: ")
+# age = input("Enter your age:")
+# number = int(input("Enter a number:"))
+
+# print("Hello " + name + "! Your age is: " + age + " The (number + 2) is: " + str(number + 2))
+
+# f strings:
+
+# print(f"Hello {name}! Your age is: {age} The (number + 2) is: {number + 2}")
+
+# print("Hello" , name , "! Your age is:" , age , "The (number + 2) is:" , number + 2)
+
+# print (number + number)
+
+# ==============================
+
+# Arithmatic Operations:
+
+# print(1+1) # 2
+# print(5-3) # 2
+# print(2*3) # 6
+# print(10 / 2) # 5
+# 
+# print(11/2) # 5.5
+# print(11 // 2) # 5
+# print(11 % 2) # 1
+# 
+# print(3 ** 5) # 3 * 3 * 3 * 3 * 3
+# print (25 ** 0.5) # 5
+
+# Priorities in Arithmatic Operations:
+
+# print( 3 * 3 + 3 / 3 - 3 ) #7
+
+# PEMDAS 
+# Parenthesis ( )  الأقواس
+# Exponent القوة **
+# Multiply, Division ضرب
+# Addition Subtraction جمع
+# Left يسار
+# Right يمين
+
+# Comparison opertators, result True or False
+# >
+# <
+# >=
+# <=
+# ==  يساوي
+# !=  لا
+
+
+# age = 19
+# # adult = True # boolean
+# adult = age > 17 # True
+# print (adult)
+
+# wealth = 1_030_010_083
+# forbidden = wealth < 1_000_000
+# 
+# print(forbidden)
+
+# number = int(input("enter a guess: "))
+# correct = number <= 11
+# print(correct)
+
+# number = int(input("enter a guess: "))
+# correct = number >= 12 and number >= 15 # 12 -> 15
+# print(correct)
+
+
+# Logical Operators
+# and
+# True and True => True
+# True and False => False
+# False and True => False
+# False and False => False
+
+# number = int(input("enter a guess: "))
+# correct = number <= 12 or number >= 15 
+# print(correct)
+
+# or
+# True or True => True
+# True or False => True
+# False or True => True
+# False or False => False
+
+# a = True
+# b = not a
+# print (b)
+
+# not
+# not True => False
+# not False => True
+
+# assignement operators:
+
+# a = 10
+# 
+# a += 10 # a = a + 1
+# a -= 3
+# a *= 2
+# a /= 2
+# a %= 17
+# 
+# b = 2
+# b **= 2
+# 
+# print(a)
+# print(b)
+
+# math functions
+# round
+
+# tomato_bill = 10.532_876
+# bill = round(tomato_bill , 2)
+# print (bill)
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
