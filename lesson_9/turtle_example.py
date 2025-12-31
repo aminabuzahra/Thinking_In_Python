a01d4cad7b41a6ac803250859a855887f56a5bd9
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2025-12-31 13:18:45 +0200

    update lesson 9

--- lesson_9/turtle_example.py
+++ lesson_9/turtle_example.py
@@ -1,13 +1,17 @@
 # import turtle
+import random
 from turtle import Turtle
 
 t = Turtle()
 
 angle = 120
 
-t.color("red")
+colors = ["red", "green", "blue", "yellow", "pink", "black", "purple", "brown"]
+
 t.speed(0)
-for i in range(37):    
+for i in range(37):
+    color = random.choice(colors)
+    t.color(color)
     t.right(10)
     for j in range(3):
         t.right(angle)
