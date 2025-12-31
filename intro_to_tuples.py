32699e046961622120a411fe240ecef648159967
AminAbuZahra <amin.abuzahra@gmail.com>
2025-12-31 22:09:57 +0200

    update lesson_10

---
+++ intro_to_tuples.py
@@ -0,0 +1,18 @@
+# tuples
+
+a = [10,20,30] # list (array)
+a[0] = 30
+print (a)
+
+a2 = tuple(a)
+print (a2)
+
+t = (1,2,3) # tuple is immutable, i.e cannot be changed!! Carved in stone
+print (len(t))
+
+t2 = list(t)
+t2.append(4)
+t2[0] = 50
+print (t2)
+
+d = { "Subhi" : 5}
